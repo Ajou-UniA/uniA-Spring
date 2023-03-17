61e351935d4409203e2762270c860cb1ab93e845
JisinKeo <tntjd9514@naver.com>
2023-03-17 13:33:41 +0900

    Add validation to MemberDTO

--- src/main/java/com/example/member/dto/MemberDTO.java
+++ src/main/java/com/example/member/dto/MemberDTO.java
@@ -3,18 +3,28 @@ package com.example.member.dto;
 import com.example.member.entity.MemberEntity;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 public class MemberDTO {
 
+    @NotBlank(message = "Please enter your name")
     private String memberName; // 이름
 
+    @NotBlank(message = "Please enter your student ID")
     private Long memberId; // 학번
 
+    @NotBlank(message = "Please enter your major")
     private String memberMajor; // 학과
 
+    @NotBlank(message = "Please enter your email")
+    @Pattern(regexp = "[a-zA-Z0-9._%+-]+@ajou\\\\.ac\\\\.kr$", message = "Email format is incorrect")
     private String memberEmail; // 이메일
 
+    @NotBlank(message = "Please enter password")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
     private String memberPassword; // 비밀번호
 
     public static MemberDTO toMemberDTO(MemberEntity memberEntity){
